0128308865c7cdfce31ff71cea81cab89d932664
Davi augusto <daviaugusto.developer@gmail.com>
2019-10-27 18:35:21 -0300

    commit inicial

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+//https://sujeitoprogramador.com/rn-api/?api-posts
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      nutri: []
+    };
+  }
+
+  componentDidMount(){
+    let url ='https://sujeitoprogramador.com/rn-api/?api=posts';
+    fetch(url)
+    .then((r)=> r.json())
+    .then((json) => {
+        let state = this.state;
+        state.nutri = json;
+        this.setState(state);
+        console.log(json);
+    })
+  }
+  render(){
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default App;
